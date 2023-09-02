5a134c9e91611938b0914d2e2fbd587ef76e3b30
gabrielbuehler1993 <gabriel.buehler@edu.teko.ch>
2023-09-02 17:31:22 +0200

    Checkboxen array in einen String umgewandelt, konnte es jetzt per E-mail senden YEEEEES (pls kill me)

--- Frontend/frontendScript.js
+++ Frontend/frontendScript.js
@@ -5,15 +5,36 @@
 })();
 //Sending the E-Mails
 function SendMail(){
+  // Create an array to store the descriptions of selected checkboxes
+  const selectedItems = [];
+
+  // Check each checkbox and add its description to the array if it's checked
+  const woodCheckbox = document.getElementById("woodCheckbox");
+  const metalDiceCheckbox = document.getElementById("metalDiceCheckbox");
+  const diceBagCheckbox = document.getElementById("diceBagCheckbox");
+
+  if (woodCheckbox.checked) {
+    selectedItems.push("Wood (150.00$)");
+  }
+
+  if (metalDiceCheckbox.checked) {
+    selectedItems.push("Metal Dice (200.00$)");
+  }
+
+  if (diceBagCheckbox.checked) {
+    selectedItems.push("Dice Bag (50.00$)");
+  }
+
+  // Include the selected items in the email parameters
   var params = {
     fname: document.getElementById("fname").value,
     email: document.getElementById("email").value,
-    woodcheckbox: document.getElementById("woodCheckbox").value,
-    metalDiceCheckbox: document.getElementById("metalDiceCheckbox").value,
-    woodcheckbox: document.getElementById("diceBagCheckbox").value
+    selectedItems: selectedItems.join(', ') // Convert the array to a comma-separated string
   }
+
+  // Send the email
   emailjs.send("service_pqk0ket", "template_aw7f345", params).then(function(res) {
-    alert("Your E-Mail has been sent, thank you for choosing D20 Shop! We will answer you shortly with the delivery details ");
+    alert("Thank you for ordering at D20 Shop. We will send you a confirmation E-Mail soon!");
   });
 }
 // Function to toggle the navigation on burger button click
